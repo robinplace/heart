c80dc58707b1af915137d240f68592c65aa057ad
Robbie Wxyz <hi@robbiew.xyz>
2020-05-01 12:34:48 -0600

    working search with people + memberships

--- public/checkin.js
+++ public/checkin.js
@@ -1,119 +1,126 @@
-const { createElement: h, Fragment, useState, useReducer, useMemo, useEffect, useCallback } = React
+const SPREADSHEET_ID = `%SPREADSHEET_ID%`
+const GAPI_INIT = {
+	clientId: `%CLIENT_ID%`,
+	apiKey: `%API_KEY%`,
+	scope: `https://www.googleapis.com/auth/spreadsheets`,
+	discoveryDocs: [ `https://sheets.googleapis.com/$discovery/rest?version=v4` ],
+}
+const FROZEN_ROWS = 1
+
+const { createStore, combineReducers, applyMiddleware } = Redux
+const { createElement: h, Fragment, useState, useReducer, useMemo, useEffect, useCallback, useRef } = React
 const { render } = ReactDOM
+const { Provider, useSelector, useDispatch } = ReactRedux
 
-const CLIENT_ID = `%CLIENT_ID%`
-const API_KEY = `%API_KEY%`
-const SCOPES = `https://www.googleapis.com/auth/spreadsheets`
-const DISCOVERY_DOCS = [ `https://sheets.googleapis.com/$discovery/rest?version=v4` ]
+const loadedReducer = (s = {}, a) => { switch (a.type) {
+	case `LOAD`: return { ...s, [a.loader]: a.loaded }
+	default: return s
+} }
+const signedInReducer = (s = null, a) => { switch (a.type) {
+	case `SIGNIN`: return a.signedIn
+	default: return s
+} }
+const syncQueueReducer = (s = [], a) => s
+const rowsReducer = (s = {}, a) => { switch (a.type) {
+	case `LOAD`: return a.rows || s
+	default: return s
+} }
+const columnsReducer = (s = {}, a) => { switch (a.type) {
+	case `LOAD`: return a.columns || s
+	default: return s
+} }
+const indexesReducer = (s = {}, a) => { switch (a.type) {
+	case `LOAD`: return a.indexes || s
+	default: return s
+} }
+const searchReducer = (s = ``, a) => { switch (a.type) {
+	case `SEARCH`: return a.search
+	default: return s
+} }
 
-const loadGapi = () => new Promise ((res, rej) => {
-	const script = document.createElement (`script`)
-	script.src = `https://apis.google.com/js/api.js`
-	script.defer = true
-	script.async = true
-	script.addEventListener (`load`, ev => res ())
-	script.addEventListener (`readystatechange`, ev => script.readyState === `complete` && res ())
-	script.addEventListener (`error`, ev => rej (ev))
-	document.body.appendChild (script)
-})
-
-const loadAuth2 = () => new Promise ((res, rej) => {
-	gapi.load (`client:auth2`, res)
-})
-
-const initGapi = () => gapi.client.init ({
-	apiKey: API_KEY,
-	clientId: CLIENT_ID,
-	discoveryDocs: DISCOVERY_DOCS,
-	scope: SCOPES,
-})
-
-const isSignedIn = onSignIn => {
-	gapi.auth2.getAuthInstance ().isSignedIn.listen (onSignIn) // listen for sign-in state changes.
-	onSignIn (gapi.auth2.getAuthInstance ().isSignedIn.get ()) // handle the initial sign-in state.
-}
-
-const initiateSignIn = () => {
-	gapi.auth2.getAuthInstance ().signIn ()
-}
-
-const initiateSignOut = () => {
-	gapi.auth2.getAuthInstance ().signOut ()
-}
-
-const MEMBERS = {
-	primaryHeadingRow: 2,
-	numberOfHeadingRows: 2,
-	headings: {
-		id: `ID`, name: `NAME`, phone: `PHONE #`,
-		plan: `PLAN`, renewalDate: `RENEWAL DATE`,
-		role: `ROLE`, issues: `ISSUES`, notes: `NOTES`,
-		cancelReason: `REASON FOR CANCELLATION`,
+const store = createStore (combineReducers ({
+	loaded: loadedReducer,
+	signedIn: signedInReducer,
+	syncQueue: syncQueueReducer,
+	rows: rowsReducer,
+	columns: columnsReducer,
+	indexes: indexesReducer,
+	search: searchReducer,
+}), {
+	loaded: {
+		local: null,
+		gapi: null,
+		auth2: null,
+		gapiInit: null,
+		spreadsheet: null,
+	},
+	signedIn: null,
+	syncQueue: [],
+	rows: {
+		people: [],
+		memberships: [],
+		plans: [],
+		attendance: [],
+		events: [],
+	},
+	columns: {
+		people: {},
+		memberships: {},
+		plans: {},
+		attendance: {},
+		events: {},
+	},
+	indexes: {
+	},
+	search: ``,
+}, applyMiddleware (
+	store => next => action => {
+		console.groupCollapsed (`action`, action.type)
+		console.log ('prev state', store.getState ())
+		console.log ('action', action)
+		try {
+			return next (action)
+		} finally {
+			console.log ('next state', store.getState ())
+			console.groupEnd ()
+		}
 	}
-}
+))
 
-const ATTENDANCE = {
-	primaryHeadingRow: 2,
-	numberOfHeadingRows: 2,
-	headings: {
-		member: `MEMBER`, date: `DATE`, eventType: `EVENT TYPE`,
-		time: `CHECK IN TIME`, notes: `NOTES`,
-	}
-}
+const parseSheet = ({ values }) => {
+	if (values.length <= FROZEN_ROWS) return { rows: [], columns: [] }
 
-const EVENTS = {
-	primaryHeadingRow: 2,
-	numberOfHeadingRows: 2,
-	headings: {
-		date: `DATE`, type: `TYPE`,
-		attendance: `ATTENDANCE`, notes: `NOTES`,
-	}
-}
-
-const parseSpreadsheet = (rows, schema) => {
-	if (rows.length <= schema.numberOfHeadingRows) return []
-
-	const keysByHeading = Object.entries (schema.headings).reduce ((obj, [ key, val ]) => ({ ...obj, [val]: key }), {})
-	const headingsByColumn = rows [schema.primaryHeadingRow - 1]
-	const keysByColumn = headingsByColumn.map (heading => keysByHeading [heading])
+	const keysByOffset = values [FROZEN_ROWS - 1]
+	// prefers the first occurrence of a key
+	const offsetsByKey = keysByOffset.reduce ((offsets, key, offset) => ({ [key]: offset, ...offsets }), {})
 
 	const data = []
-	for (let i = schema.numberOfHeadingRows; i < rows.length; i++) {
-		const row = rows [i]
+	for (let i = FROZEN_ROWS; i < values.length; i++) {
+		const row = values [i]
 		if (row.length === 0) continue
-		data.push (row.reduce ((obj, value, column) => (keysByColumn [column] ? { ...obj, [keysByColumn [column]]: value } : obj), {}))
+		data.push (row.reduce ((data, value, offset) => {
+			const key = keysByOffset [offset]
+			if (!key) return data
+			// again, prefer the first occurrence
+			return { [key]: value, ...data }
+		}, { index: data.length, row: i + 1 }))
 	}
 
-	return data
+	return { rows: data, columns: offsetsByKey }
 }
 
 const indexBy = (rows, key) => {
 	const object = {}
 	rows.forEach (row => {
 		const index = row [key]
-		if (object [index]) object [index].push (row)
-		else object [index] = [ row ]
+		if (object [index]) object [index].push (row.index)
+		else object [index] = [ row.index ]
 	})
 	return object
 }
 
-const loadData = () => gapi.client.sheets.spreadsheets.values.batchGet ({
-	spreadsheetId: `%SPREADSHEET_ID%`,
-	ranges: [
-		`Members!A:ZZ`,
-		`Attendance!A:ZZ`,
-	],
-}).then (response => {
-	const ranges = response.result.valueRanges
-	const members = parseSpreadsheet (ranges [0].values, MEMBERS)
-	const attendanceRows = parseSpreadsheet (ranges [1].values, ATTENDANCE)
-	const attendance = indexBy (attendanceRows, `member`)
-	console.log ({ members, attendance })
-	return { members, attendance }
-})
-
 const appendAttendance = (member, date, time) => gapi.client.sheets.spreadsheets.values.append ({
-	spreadsheetId: `%SPREADSHEET_ID%`,
+	spreadsheetId: SPREADSHEET_ID,
 	range: `Attendance!A:ZZ`,
 	valueInputOption: `USER_ENTERED`,
 	insertDataOption: `INSERT_ROWS`,
@@ -123,23 +130,6 @@ const appendAttendance = (member, date, time) => gapi.client.sheets.spreadsheets
 	] },
 }).then (response => {
 })
-
-const useLoad = (loader, deps) => {
-	const [ loaded, setLoaded ] = useState (false)
-	useMemo (() => {
-		for (let i = 0; i < deps.length; i++) if (!deps [i]) {
-			if (loaded) setLoaded (false)
-			return
-		}
-		const promise = loader ()
-		if (!promise) {
-			setLoaded (true)
-			return
-		}
-		promise.then (() => setLoaded (true), console.error)
-	}, deps)
-	return loaded
-}
 
 const toDate = timestamp => {
 	const date = new Date (timestamp - 1000 * 60 * 60 * 4)
@@ -151,96 +141,272 @@ const toTime = timestamp => {
 	return `${(time.getHours () % 12 || 12) + 1}:${time.getMinutes ()}:${time.getSeconds ()} ${time.getHours () < 12 ? `AM` : `PM`}`
 }
 
-const fromTime = time => new Date (`${time} ${dateNow ()}`) * 1
+const fromTime = time => new Date (`${time} ${todayDate ()}`) * 1
 const fromDate = date => new Date (`00:00:00 ${date}`) * 1
 
-const dateToday = () => toDate (Date.now () - 1000 * 60 * 60 * 4)
-const timeNow = () => toTime (Date.now ())
+const todayDate = () => toDate (Date.now () - 1000 * 60 * 60 * 4)
+const nowTime = () => toTime (Date.now ())
 
-const SearchRow = ({ member, dates = [], dispatch }) => {
-	const hereToday = useCallback (() => {
+const ordinal = n => n + ([,'st','nd','rd'][n%100>>3^1&&n%10]||'th')
+
+const Wrapper = () => {
+	return h (Fragment, {}, [
+		h (LocalLoader),
+		h (LocalListener),
+		h (GapiLoader),
+		h (Auth2Loader),
+		h (GapiInitLoader),
+		h (SignInListener),
+		h (SpreadsheetLoader),
+		h (App),
+	])
+}
+
+const Loader = ({ loader, ready, promise, retry = true }) => {           
+	const dispatch = useDispatch ()
+	const loaded = useSelector (s => s.loaded [loader])
+	const interval = useRef ()
+	useEffect (() => {
+		if (ready === true && loaded === null) promise ().then (
+			payload => dispatch ({ type: `LOAD`, loader, loaded: true, ...payload }),
+			error => dispatch ({ type: `LOAD`, loader, loaded: false, error }))
+		if (ready === true && loaded === false && retry) interval.current = setInterval (
+			() => dispatch ({ type: `LOAD`, loader, loaded: null }), 2000)
+		return () => clearInterval (interval.current)
+	}, [ ready, loaded ])
+	return null
+}
+
+const LocalLoader = () => h (Loader, {
+	loader: `local`, ready: true, retry: false,
+	promise: () => {
+		return new Promise ((res, rej) => {
+			const state = localStorage.getItem (`state`)
+			if (!state) rej ()
+			else res (JSON.parse (state))
+		})
+	}
+})
+
+const LocalListener = () => {
+	const state = useSelector (s => s)
+	useEffect (() => {
+		localStorage.setItem (`state`, JSON.stringify (state))
+	}, [ state ])
+	return null
+}
+
+const GapiLoader = () => h (Loader, {
+	loader: `gapi`, ready: true,
+	promise: () => new Promise ((res, rej) => {
+		const script = document.createElement (`script`)
+		script.src = `https://apis.google.com/js/api.js`
+		script.defer = true
+		script.async = true
+		script.addEventListener (`load`, ev => res ())
+		script.addEventListener (`readystatechange`, ev => script.readyState === `complete` && res ())
+		script.addEventListener (`error`, ev => rej (ev))
+		document.body.appendChild (script)
+	})
+})
+
+const Auth2Loader = () => h (Loader, {
+	loader: `auth2`, ready: useSelector (s => s.loaded.gapi),
+	promise: () => new Promise (res => gapi.load (`client:auth2`, res))
+})
+
+const GapiInitLoader = () => h (Loader, {
+	loader: `gapiInit`, ready: useSelector (s => s.loaded.auth2),
+	promise: () => gapi.client.init (GAPI_INIT)
+})
+
+const SignInListener = () => {
+	const ready = useSelector (s => s.loaded.gapiInit)
+	const dispatch = useDispatch ()
+	const onSignIn = useCallback (signedIn => {
+		dispatch ({ type: `SIGNIN`, signedIn })
+	}, [ dispatch ])
+	useEffect (() => {
+		if (ready === true) {
+			gapi.auth2.getAuthInstance ().isSignedIn.listen (onSignIn) // listen for sign-in state changes.
+			onSignIn (gapi.auth2.getAuthInstance ().isSignedIn.get ()) // handle the initial sign-in state.
+		}
+	}, [ ready, onSignIn ])
+	return null
+}
+
+const SpreadsheetLoader = () => h (Loader, {
+	loader: `spreadsheet`, ready: useSelector (s => s.signedIn),
+	promise: () => gapi.client.sheets.spreadsheets.values.batchGet ({
+		spreadsheetId: SPREADSHEET_ID,
+		ranges: [
+			`People!A:ZZ`,
+			`Memberships!A:ZZ`,
+			`Plans!A:ZZ`,
+			`Attendance!A:ZZ`,
+			`Events!A:ZZ`,
+		],
+	}).then (response => {
+		const ranges = response.result.valueRanges
+		const [ rows, columns ] = [
+			`people`, `memberships`, `plans`, `attendance`, `events`
+		].reduce (([ rows, columns ], name, i) => {
+			const sheet = parseSheet (ranges [i])
+			return [
+				{ ...rows, [name]: sheet.rows },
+				{ ...columns, [name]: sheet.columns },
+			]
+		}, [ {}, {} ])
+		const indexes = {
+			peopleById: indexBy (rows.people, `id`),
+			membershipsByPerson: indexBy (rows.memberships, `person`),
+			plansById: indexBy (rows.plans, `id`),
+			attendanceByPerson: indexBy (rows.attendance, `person`),
+			attendanceByDate: indexBy (rows.attendance, `date`),
+			eventsByDate: indexBy (rows.events, `date`),
+		}
+		return { rows, columns, indexes }
+	})
+})
+
+const App = () => {
+	return h (Fragment, {}, [
+		h (Topbar),
+		h (Search),
+	])
+}
+
+const Topbar = () => {
+	return h (`div`, { class: `Topbar` }, [
+		h (SearchBox),
+		h (Indicator),
+		h (SearchHead),
+	])
+}
+const SearchBox = () => {
+	const dispatch = useDispatch ()
+	const search = useSelector (s => s.search)
+	const setSearch = useCallback (ev => dispatch ({ type: `SEARCH`, search: ev.target.value }), [ dispatch ])
+	return h (`div`, { class: `SearchBox` }, [
+		h (`input`, { class: `SearchInput`, placeholder: `Search by name or phone #`, onInput: setSearch, value: search }),
+		h (`button`, {}, `Add person`),
+	])
+}
+
+const Indicator = () => {
+	const signedIn = useSelector (s => s.signedIn)
+	const signIn = useCallback (() => gapi.auth2.getAuthInstance ().signIn ())
+	const signOut = useCallback (() => gapi.auth2.getAuthInstance ().signOut ())
+	const loaded = useSelector (s => s.loaded)
+
+	return h (`span`, { class: `Indicator` }, [
+		h (`span`, { class: `IndicatorLoading` }, h (IndicatorLoading, { signedIn, loaded })),
+		signedIn === false && h (`button`, { onClick: signIn }, `Sign in` ),
+		signedIn === true && h (`button`, { onClick: signOut }, `Sign out` ),
+	])
+}
+
+const IndicatorLoading = ({ signedIn, loaded }) => {
+	if (!loaded.local) return `Loading cache`
+	if (!loaded.gapi) return `Loading gapi`
+	if (!loaded.auth2) return `Loading auth2 api`
+	if (!loaded.gapi) return `Initing gapi`
+	if (signedIn === null) return `Loading sign in`
+	if (!loaded.spreadsheet) return `Loading data`
+	return null
+}
+
+const SearchHead = () => {
+	return h (`div`, { class: `Head PersonRow` }, [
+		h (`span`, { class: `Cell PersonName` }, `Name`),
+		h (`span`, { class: `Cell PersonPhone` }, `Phone`),
+		h (`span`, { class: `Cell PersonRole` }, `Role`),
+		h (`span`, { class: `Cell PersonNote` }, `Note`),
+		h (`div`, { class: `PersonMemberships` }, [
+			h (`div`, { class: `Row MembershipRow`, current: `true` }, [
+				h (`span`, { class: `Cell MembershipPlan` }, `Plan`),
+				h (`span`, { class: `Cell MembershipStart` }, `Start`),
+				h (`span`, { class: `Cell MembershipEnd` }, `End`),
+				h (`span`, { class: `Cell MembershipRenewal` }, ``),
+				h (`span`, { class: `Cell MembershipProblem` }, `Check in`),
+			]),
+		]),
+	])
+}
+
+const Search = () => {
+	const people = useSelector (s => s.rows.people)
+	const search = useSelector (s => s.search)
+	const matches = people.filter (person => {
+		if (person.name.toLowerCase ().indexOf (search.toLowerCase ()) !== -1) return true
+		if (person.phone.indexOf (search) !== -1) return true
+		return false
+	})
+	return h (`div`, { class: `Search` }, [
+		...matches.slice (0, 20).map (({ index }) => {
+			return h (PersonRow, { key: index, index })
+		})
+	])
+}
+
+const PersonRow = ({ index }) => {
+	const person = useSelector (s => s.rows.people [index])
+	const attendanceIndexes = useSelector (s => s.indexes.attendanceByPerson [person.id] || [])
+
+	/*const hereToday = useCallback (() => {
 		const date = { member: member.id, date: dateToday (), time: timeNow () }
 		appendAttendance (member.id, date.date, date.time).then (() => {
 			dispatch ({ type: `HERE_TODAY`, member: member.id, date })
 		}, console.error)
-	}, [ member.id ])
+	}, [ member.id ])*/
 
-	return h (`div`, {}, [
-		member.name,
-		h (`div`, {}, [
-			dates.map (date => date.date).join (`, `),
-		]),
-		h (`br`),
-		dates.find (date => date.date === dateToday ()) ? h (`button`, { disabled: true }, `Already checked in`) : h (`button`, { onClick: hereToday }, `Check in today`),
-		h (`hr`),
+	return h (`div`, { class: `Row PersonRow` }, [
+		h (`span`, { class: `Cell PersonName` }, person.name),
+		h (`span`, { class: `Cell PersonPhone` }, person.phone),
+		h (`span`, { class: `Cell PersonRole` }, person.role),
+		h (`span`, { class: `Cell PersonNote` }, person.note),
+		h (`button`, { class: `Cell PersonEdit` }, `\u270F\uFE0F`),
+		h (`button`, { class: `Cell PersonEdit` }, `Add membership`),
+		h (PersonMemberships, { id: person.id }),
 	])
 }
 
-const SearchBox = ({ members, attendance, dispatch }) => {
-	const [ search, onSearch ] = useState (``)
-	const onChange = useCallback (ev => onSearch (ev.target.value), [ onSearch ])
+const PersonMemberships = ({ id }) => {
+	const membershipIndexes = useSelector (s => s.indexes.membershipsByPerson [id] || [])
+	const memberships = useSelector (s => membershipIndexes.map (i => s.rows.memberships [i]))
+	const sortedMemberships = memberships.sort ((a, b) => {
+		a = fromDate (a.start), b = fromDate (b.start)
+		return a < b ? 1 : a > b ? -1 : 0
+	})
 
-	const matches = search.length >= 1 ? members.filter (member => {
-		if (member.name.toLowerCase ().indexOf (search.toLowerCase ()) !== -1) return true
-		if (member.phone.indexOf (search) !== -1) return true
-		return false
-	}) : []
-
-	return [
-		h (`input`, { placeholder: `Name or phone #`, onChange, value: search }),
-		...matches.slice (0, 10).map (member => h (SearchRow, { key: member.id, member, dates: attendance [member.id], dispatch })),
-	]
+	return h (`div`, { class: `PersonMemberships` }, sortedMemberships.map (({ index }) => {
+		return h (MembershipRow, { key: index, index })
+	}))
 }
 
-const Search = ({ data, dispatch }) => {
-	const { members, attendance } = data
-	return [
-		`Search ${members.length} membership records here`,
-		h (`button`, { onClick: initiateSignOut }, `Sign out` ),
-		h (`hr`),
-		h (SearchBox, { members, attendance, dispatch }),
-	]
-}
+const MembershipRow = ({ index }) => {
+	const membership = useSelector (s => s.rows.memberships [index])
+	const current = !membership.end || fromDate (membership.end) < todayDate ()
+	const canCheckIn = !!current && !membership.problem
 
-const reducer = (state, action) => { switch (action.type) {
-	case `LOAD`:
-		return {
-			...state,
-			loaded: true, members: action.data.members, attendance: action.data.attendance
-		}
-	case `HERE_TODAY`:
-		return {
-			...state,
-			attendance: {
-				...state.attendance,
-				[action.member]: [ ...state.attendance [action.member] || [], action.date ],
-			}
-		}
-	default:
-		throw new Error (`${action.type} is an invalid action type to dispatch`)
-} }
-
-const Wrapper = () => {
-	const gapiLoaded = useLoad (loadGapi, [])
-	const auth2Loaded = useLoad (loadAuth2, [ gapiLoaded ])
-	const gapiInited = useLoad (initGapi, [ auth2Loaded ])
-	const [ signedIn, setSignedIn ] = useState (false)
-	useLoad (() => isSignedIn (setSignedIn), [ gapiInited ])
-	const [ data, dispatch ] = useReducer (reducer, { loaded: false })
-	useLoad (() => loadData ().then (data => dispatch ({ type: `LOAD`, data })), [ signedIn ])
-
-	if (!gapiLoaded) return `Loading gapi`
-	if (!auth2Loaded) return `Loading auth2 api`
-	if (!gapiInited) return `Initing gapi`
-	if (!signedIn) return [ `Sign in now`, h (`button`, { onClick: initiateSignIn }, `Sign in` ) ]
-	if (!data.loaded) return `Loading data`
-	return h (Search, { data, dispatch })
+	return h (`div`, { class: `Row MembershipRow`, current: current ? `true` : null }, [
+		h (`span`, { class: `Cell MembershipPlan` }, membership.plan),
+		h (`span`, { class: `Cell MembershipStart` }, membership.start),
+		h (`span`, { class: `Cell MembershipEnd` }, membership.end),
+		h (`span`, { class: `Cell MembershipRenewal` }, membership.renewal && `(${ordinal (membership.renewal)})`),
+		canCheckIn || h (`span`, { class: `Cell MembershipProblem` }, membership.problem),
+		canCheckIn && h (`span`, { class: `Cell MembershipChecks` }, [
+			h (`button`, { class: `Cell MembershipCheckIn` }, `Check in`),
+		]),
+		h (`button`, { class: `Cell MembershipEdit` }, `\u270F\uFE0F`),
+	])
 }
 
 document.addEventListener (`readystatechange`, ev => {
 	if (document.readyState === `interactive`) {
 		const wrapper = document.createElement (`div`)
-		render (h (Wrapper), wrapper)
+		wrapper.setAttribute (`class`, `Wrapper`)
+		render (h (Provider, { store }, h (Wrapper)), wrapper)
 		document.body.appendChild (wrapper)
 	}
 })
