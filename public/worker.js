13cacbe4f30cece7b814139883ef82b726a18c63
Robbie Wxyz <hi@robbiew.xyz>
2020-06-13 20:26:03 -0600

    correct service worker js asset references

--- public/worker.js
+++ public/worker.js
@@ -1,4 +1,4 @@
-const VERSION = `v7`
+const VERSION = `v10`
 
 self.addEventListener (`install`, ev => ev.waitUntil (
 	caches.open (VERSION)
@@ -7,21 +7,22 @@ self.addEventListener (`install`, ev => ev.waitUntil (
 		`./checkin.html`,
 		`./checkin.js`,
 		`./date_fns.min.js`,
-		`./redux.min.js`,
-		`./react.min.js`,
-		`./react-dom.min.js`,
-		`./react-redux.min.js`,
+		`./redux.js`,
+		`./react.js`,
+		`./react-dom.js`,
+		`./react-redux.js`,
 	]))
+))
+
+self.addEventListener (`activate`, ev => ev.waitUntil (
+	caches.keys ().then (keys => Promise.all (keys.map (key => {
+		console.log (key, key !== VERSION)
+		if (key !== VERSION) return caches.delete (key)
+	})))
 ))
 
 self.addEventListener (`fetch`, ev => ev.respondWith (
 	caches.open (VERSION)
 	.then (cache => cache.match (ev.request))
 	.then (res => res || fetch (ev.request))
-))
-
-self.addEventListener (`activate`, ev => ev.waitUntil (
-	caches.keys ().then (keys => Promise.all (keys.map (key => {
-		if (key !== VERSION) return caches.delete (key)
-	})))
 ))
